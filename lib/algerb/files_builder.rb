23966ccf374de767bd1842e28b351006fd0f0003
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-12-31 03:19:57 +0900

    Add: Algerb::FilesBuilder#add

--- lib/algerb/files_builder.rb
+++ lib/algerb/files_builder.rb
@@ -5,11 +5,20 @@ module Algerb; end
 class Algerb::FilesBuilder
   attr_reader :files
 
+  FILE_PATTERN = %r{^([a-z0-9\_\.])/?}
+
   def initialize
     @files = Algerb::Files.new
   end
 
   def add(path)
-    @files.add(Algerb::File.new(path))
+    paths = path.split('/').reverse
+    file = Algerb::File.new(paths.slice!(0, 1)[0])
+    files = paths.inject(file) do |file, path|
+      files = Algerb::Files.new(path).add(file)
+      files
+    end
+    @files.add files
+    self
   end
 end
--- spec/unit/algerb/files_builder_spec.rb
+++ spec/unit/algerb/files_builder_spec.rb
@@ -15,5 +15,10 @@ describe Algerb::FilesBuilder do
       before { builder.add('foo.rb') }
       it { should == Algerb::Files.new.add(Algerb::File.new('foo.rb'))}
     end
+
+    context 'when 1 file ("foo/bar.rb") is added' do
+      before { builder.add('foo/bar.rb') }
+      it { should == Algerb::Files.new.add(Algerb::Files.new('foo').add(Algerb::File.new('bar.rb')))}
+    end
   end
 end
