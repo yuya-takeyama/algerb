372b2c91ef77b0166813b65cb66d4d586f127c80
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-12-31 21:31:26 +0900

    Fix: Ruby 1.8.7 adaption.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,4 @@ $LOAD_PATH.unshift(File.dirname(__FILE__) + '/../lib')
 require 'bundler'
 Bundler.setup(:default, :development)
 require 'rspec'
-require File.realpath(File.dirname(__FILE__)) + '/../autoloader.rb'
+require File.expand_path(File.dirname(__FILE__)) + '/../autoloader.rb'
