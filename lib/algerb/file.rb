8b1f88079871c6566e1676855c3a3d7122a15d65
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-01-02 14:08:38 +0900

    Remove: Derprecated code.

--- lib/algerb/file.rb
+++ lib/algerb/file.rb
@@ -1,19 +1,13 @@
 class Algerb::File
   include Algerb::Util
 
-  FILE_PATTERN = %r{/?(a-z0-9\-_\.)$}i
-
   attr_reader :name
 
   def initialize(name)
     @name = name
   end
 
-  def each(&block)
-    each_files(&block)
-  end
-
   def ==(another)
-    self.name == another.name
+    name == another.name
   end
 end
